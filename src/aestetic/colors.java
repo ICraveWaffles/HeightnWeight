cf44d6545a449991d92fb1f9b672104c67348e1b
Louis_Pisvin <141773625+ICraveWaffles@users.noreply.github.com>
2025-10-02 08:57:45 +0200

    Hm

---
+++ src/aestetic/colors.java
@@ -0,0 +1,28 @@
+package aestetic;
+
+import processing.core.PApplet;
+
+public class colors {
+
+    public int [] colors;
+
+    public colors (PApplet p5){
+        this.instanceColors(p5);
+    }
+
+    public void instanceColors(PApplet p5) {
+        colors = new int[8];
+        colors[0] = p5.color(0XFF414141);
+        colors[1] = p5.color(0XFF534559);
+        colors[2] = p5.color(0XFFF4F0F7);
+        colors[3] = p5.color(0XFF440D6E);
+        colors[4] = p5.color(0XFF7D67E3);
+        colors[5] = p5.color(0XFFEED6FF);
+        colors[6] = p5.color(0XFF000000);
+        colors[7] = p5.color(0XFFFFFFFF);
+    }
+
+    public int getThisColor(int i){
+        return this.colors[i];
+    }
+}
---
+++ src/aestetic/fontsizes.java
@@ -0,0 +1,9 @@
+package aestetic;
+
+public class fontsizes {
+
+    public static final float titleSize = 40;
+    public static final float textSize = 30;
+    public static final float buttonTextSize = 24;
+    public static final float parrafadaSize = 16;
+}
