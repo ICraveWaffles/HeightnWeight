f9d930a2a3e195b57a33fe0ba03b1351f435965c
Louis_Pisvin <141773625+ICraveWaffles@users.noreply.github.com>
2025-10-16 09:49:23 +0200

    Buttons

---
+++ src/Button.java
@@ -0,0 +1,76 @@
+import processing.core.PApplet;
+
+public class Button {
+
+
+    float x, y, w, h;
+    int fillColor, strokeColor;
+    int fillColorOver, fillColorDisabled;
+    String bText;
+    public boolean enabled;
+
+    public Button(PApplet p5, String text, float x, float y, float w, float h, int f, int s, int t){
+        this.bText = text;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.enabled = true;
+        this.fillColor = Colors.getThisColor(f);
+        this.fillColorOver = Colors.getThisColor(f+1);
+        this.fillColorDisabled = Colors.getThisColor(0);
+        this.strokeColor = Colors.getThisColor(s);
+    }
+
+    public void setEnabled(boolean b){
+        this.enabled = b;
+    }
+
+    public void setbText(String t){ this.bText = t; }
+
+    public void setColors(int cFill, int cStroke, int cOver, int cDisabled){
+        this.fillColor = cFill;
+        this.strokeColor = cStroke;
+        this.fillColorOver = cOver;
+        this.fillColorDisabled = cDisabled;
+    }
+
+    public boolean isEnabled(){
+        return  this.enabled;
+    }
+
+
+    public void display(PApplet p5){
+        p5.pushStyle();
+
+        p5.rectMode(p5.CENTER);
+        p5.textAlign(p5.CENTER, p5.CENTER);
+
+        if(!enabled){
+            p5.fill(fillColorDisabled);
+        }
+        else if(mouseOverButton(p5)){
+            p5.fill(fillColorOver);
+        }
+        else{
+            p5.fill(fillColor);
+        }
+        p5.stroke(strokeColor); p5.strokeWeight(2);
+        p5.rect(this.x, this.y, this.w, this.h, 10);
+
+
+        p5.fill(Colors.getThisColor(7)); p5.textFont(Fonts.getThisFont(1));
+        p5.text(bText, this.x , this.y);
+        p5.popStyle();
+    }
+
+    public boolean mouseOverButton(PApplet p5){
+        return (p5.mouseX >= this.x-this.w/2) && (p5.mouseX <= this.x + this.w/2) &&
+                (p5.mouseY >= this.y-this.h/2) && (p5.mouseY <= this.y + this.h/2);
+    }
+
+    // Indica si cal posar el cursor a HAND
+    public boolean updateHandCursor(PApplet p5){
+        return mouseOverButton(p5) && enabled;
+    }
+}
